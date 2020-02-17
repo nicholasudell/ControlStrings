f041d9cc358cfaea060bd522f8fd494d8258acf0
Nick Udell <nicholas.udell@qms-uk.com>
2020-02-17 11:36:01 +0000

    Adds a runner for Example.cs

---
+++ Runner/Program.cs
@@ -0,0 +1,21 @@
+﻿using ControlStrings;
+using System;
+using System.Threading;
+
+namespace Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(new ControlStrings.Example.Example().ToString());
+                Console.WriteLine("================");
+            }
+        }
+    }
+}
