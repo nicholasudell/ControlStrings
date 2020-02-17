a9f0d820d2aaee16fe3a223b233a8a7a2033c3d0
Nick Udell <nicholas.udell@qms-uk.com>
2020-02-17 14:14:05 +0000

    Adds transformer that upper-cases the first char

---
+++ ControlStrings/Transformers/StartUpperTransformer.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+
+namespace ControlStrings
+{
+    public class StartUpperTransformer : ITransformer
+    {
+        public bool Matches(string transformString) => transformString.Equals("StartUpper");
+        public string Transform(string input) => input.First().ToString().ToUpper() + input.Substring(1);
+    }
+}
