876916cb9e8553953cca821342e62da73c5d99c5
Nick Udell <nicholas.udell@qms-uk.com>
2020-02-18 16:02:08 +0000

    Adds tests for the contextcontrolstringmatcher and the collectioncontrolstringmatcher

---
+++ Unit Tests/TestHelpers.cs
@@ -0,0 +1,17 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.NUnit3;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControlStrings.UnitTests
+{
+    internal class AutoMoqDataAttribute : AutoDataAttribute
+    {
+        internal AutoMoqDataAttribute()
+            : base(()=> new Fixture().Customize(new AutoMoqCustomization()))
+        {
+        }
+    }
+}
