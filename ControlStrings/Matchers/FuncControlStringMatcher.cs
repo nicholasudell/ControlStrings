cef3153ef3283239cdfc687ca6005cc848218910
Nick Udell <nicholas.udell@qms-uk.com>
2020-02-20 13:17:43 +0000

    Revert "Test commit with a warnable offence for ReviewDog"
    This reverts commit 0d6f8cd5d7e403325b5c9642d32ae5965ae3e2eb.

--- ControlStrings/Matchers/FuncControlStringMatcher.cs
+++ ControlStrings/Matchers/FuncControlStringMatcher.cs
@@ -21,10 +21,9 @@ namespace ControlStrings
                 throw new ArgumentException("Argument cannot be matched by this matcher.", nameof(controlString));
             }
 
-            var a = true;
-
             return Matcher(controlString);
         }
+
         public bool Matches(ControlString controlString) => CanMatch(controlString);
     }
 }
